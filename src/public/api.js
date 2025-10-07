4bc5bb3947264065a9b4d10b5eff87e5d1b44a2d
andejuli <techtrek@hotmail.com>
2025-10-07 13:50:46 -0600

    move api.html

---
+++ src/public/api.js
@@ -0,0 +1,45 @@
+// api.js
+const baseUrl = "https://developer.nps.gov/api/v1/";
+
+async function getJson(endpoint) {
+  // replace this with your actual key
+  //const apiKey = 'qEtzgv9y4G8bS0IY3Kk0faqLvkvuxETgxycqvzFQ';
+  const apiKey = import.meta.env.VITE_NPS_API_KEY;
+  // construct the url: baseUrl + endpoint + parameters
+  const url = baseUrl + endpoint;
+  // set the options. The important one here is the X-Api-Key
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": apiKey
+      }
+  }
+  // make the request
+  const response = await fetch(url, options)
+  const data = await response.json()
+  console.log(data)
+  return data;
+}
+
+//getJson('alerts?parkCode=acad,dena');
+
+let output = document.querySelector('#output');
+
+
+
+function listTemplate(item) {
+  return `<li><a href="${item.url}">${item.fullName}</a> ${item.states}</li>`
+}
+
+async function renderClimbingList() {
+  const endpoint = "activities/parks?q=climbing"
+  const listElement = document.getElementById("outputList")
+  const data = await getJson(endpoint)
+  console.log(data);
+  const parks = data.data[0].parks;
+  console.log(parks);
+  const listHtml = parks.map(listTemplate).join("")
+  listElement.innerHTML = listHtml;
+}
+renderClimbingList()
